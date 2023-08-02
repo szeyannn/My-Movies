46dab24ada40801ddfe75a1a9c172a777d66e361
TE(TING) SZE YAN <22026408@myrp.edu.sg>
2023-08-02 09:01:33 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22026408/mymovies/MainActivity.java
@@ -0,0 +1,57 @@
+package sg.edu.rp.c346.id22026408.mymovies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etTitle, etGenre, etYear;
+    Spinner spinnerRating;
+    Button btnInsert, btnShow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etTitle = findViewById(R.id.etTitle);
+        etGenre = findViewById(R.id.etGenre);
+        etYear = findViewById(R.id.etYear);
+        spinnerRating = findViewById(R.id.spinner);
+        btnInsert = findViewById(R.id.buttonInsert);
+        btnShow = findViewById(R.id.buttonShow);
+
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper db = new DBHelper(MainActivity.this);
+                String selectedItem = spinnerRating.getSelectedItem().toString();
+                db.insertMovie(etTitle.getText().toString(), etGenre.getText().toString(), Integer.parseInt(etYear.getText().toString()), selectedItem);
+                Toast.makeText(MainActivity.this, "Movie Added", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DisplayActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
